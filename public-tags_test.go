1c836e4eb0e095a34beef971fee999d89a6b6de7
Kostis Kapelonis <kostis@codefresh.io>
2020-04-26 22:26:23 +0300

    Added more unit tests

--- public-tags_test.go
+++ public-tags_test.go
@@ -16,6 +16,24 @@ func TestUnitDiscardInvalid(t *testing.T) {
 
 }
 
+func TestUnitNewest(t *testing.T) {
+
+	var allTags = []string{"2.1.1", "2.4.34", "1.9.12", "latest", "pre-2"}
+
+	newest := findNewestSemanticTag(allTags)
+
+	assert.Equal(t, "2.4.34", newest, "Newest tag was not found")
+}
+
+func TestUnitwithZeroVersions(t *testing.T) {
+
+	var allTags = []string{"0.11.1", "0.24", "0.9.12", "latest", "0.1.2"}
+
+	newest := findNewestSemanticTag(allTags)
+
+	assert.Equal(t, "0.11.1", newest, "Newest tag was not found")
+}
+
 // func TestReadMediumDataset(t *testing.T) {
 
 // 	stepsFound := readJSON("testdata/input-medium.json")
